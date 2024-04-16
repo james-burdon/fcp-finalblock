e37a7fa1fdf0bf5cbde25f9a7a43ac7df110955c
james-burdon <eq23636@bristol.ac.uk>
2024-04-16 14:34:19 +0100

    update exercice 1 done

---
+++ task3.py
@@ -0,0 +1,95 @@
+class Node:
+
+	def __init__(self, value, number, connections=None):
+
+		self.index = number
+		self.connections = connections
+		self.value = value
+
+class Network: 
+
+	def __init__(self, nodes=None):
+
+        if nodes is None:
+            self.nodes = []
+        else:
+            self.nodes = nodes 
+
+	def get_mean_degree(self):
+		total=0
+		for i in range(len(self.nodes)):
+			total1=0
+			for j in range(len(self.nodes[i].connections)):
+				total1+=self.nodes[i].connections
+			total+=total1
+		return total/len(self.nodes)
+
+
+	def get_mean_clustering(self):
+		#Your code for task 3 goes here
+
+	def get_mean_path_length(self):
+		#Your code for task 3 goes here
+
+	def make_random_network(self, N, connection_probability):
+		'''
+		This function makes a *random* network of size N.
+		Each node is connected to each other node with probability p
+		'''
+
+		self.nodes = []
+		for node_number in range(N):
+			value = np.random.random()
+			connections = [0 for _ in range(N)]
+			self.nodes.append(Node(value, node_number, connections))
+
+		for (index, node) in enumerate(self.nodes):
+			for neighbour_index in range(index+1, N):
+				if np.random.random() < connection_probability:
+					node.connections[neighbour_index] = 1
+					self.nodes[neighbour_index].connections[index] = 1
+
+	def plot(self):
+
+		fig = plt.figure()
+		ax = fig.add_subplot(111)
+		ax.set_axis_off()
+
+		num_nodes = len(self.nodes)
+		network_radius = num_nodes * 10
+		ax.set_xlim([-1.1*network_radius, 1.1*network_radius])
+		ax.set_ylim([-1.1*network_radius, 1.1*network_radius])
+
+		for (i, node) in enumerate(self.nodes):
+			node_angle = i * 2 * np.pi / num_nodes
+			node_x = network_radius * np.cos(node_angle)
+			node_y = network_radius * np.sin(node_angle)
+
+			circle = plt.Circle((node_x, node_y), 0.3*num_nodes, color=cm.hot(node.value))
+			ax.add_patch(circle)
+
+			for neighbour_index in range(i+1, num_nodes):
+				if node.connections[neighbour_index]:
+					neighbour_angle = neighbour_index * 2 * np.pi / num_nodes
+					neighbour_x = network_radius * np.cos(neighbour_angle)
+					neighbour_y = network_radius * np.sin(neighbour_angle)
+
+					ax.plot((node_x, neighbour_x), (node_y, neighbour_y), color='black')
+
+def test_networks():
+
+	#Ring network
+	nodes = []
+	num_nodes = 10
+	for node_number in range(num_nodes):
+		connections = [0 for val in range(num_nodes)]
+		connections[(node_number-1)%num_nodes] = 1
+		connections[(node_number+1)%num_nodes] = 1
+		new_node = Node(0, node_number, connections=connections)
+		nodes.append(new_node)
+	network = Network(nodes)
+
+	print("Testing ring network")
+	assert(network.get_mean_degree()==2), network.get_mean_degree()
+	assert(network.get_clustering()==0), network.get_clustering()
+	assert(network.get_path_length()==2.777777777777778), network.get_path_length()
